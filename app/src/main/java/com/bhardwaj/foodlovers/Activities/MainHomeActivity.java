40176f39873afd0f69288621833683ac3bbb294e
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2020-06-07 19:27:57 +0530

    - Navigation fixed. Now fragment is not recreated.

--- app/src/main/java/com/bhardwaj/foodlovers/Activities/MainHomeActivity.java
+++ app/src/main/java/com/bhardwaj/foodlovers/Activities/MainHomeActivity.java
@@ -1,10 +1,14 @@
 package com.bhardwaj.foodlovers.Activities;
 
+import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.MenuItem;
 
 import com.bhardwaj.foodlovers.Fragments.CartFragment;
@@ -15,38 +19,37 @@ import com.bhardwaj.foodlovers.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainHomeActivity extends AppCompatActivity {
-
+    HomeFragment homeFragment = new HomeFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    CartFragment cartFragment = new CartFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
         BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+
+
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id = menuItem.getItemId();
                 if (id == R.id.navigation_home) {
-                    HomeFragment fragment = new HomeFragment();
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.mainFragment, fragment);
-                    transaction.commit();
+                    displayHome();
+                    return true;
                 } else if (id == R.id.navigation_search) {
-                    SearchFragment fragment = new SearchFragment();
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.mainFragment, fragment);
-                    transaction.commit();
+                    displaySearch();
+                    return true;
                 } else if (id == R.id.navigation_cart) {
-                    CartFragment fragment = new CartFragment();
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.mainFragment, fragment);
-                    transaction.commit();
+                    displayCart();
+                    return true;
                 } else if (id == R.id.navigation_profile) {
-                    ProfileFragment fragment = new ProfileFragment();
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    transaction.replace(R.id.mainFragment, fragment);
-                    transaction.commit();
+                    displayProfile();
+                    return true;
+                } else {
+                    return false;
                 }
-                return true;
+
             }
         });
 
@@ -56,5 +59,81 @@ public class MainHomeActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    private void displayHome() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (homeFragment.isAdded()) {
+            ft.show(homeFragment);
+        } else {
+            ft.add(R.id.mainFragment, homeFragment);
+        }
+        if (searchFragment.isAdded()) {
+            ft.hide(searchFragment);
+        }
+        if (cartFragment.isAdded()) {
+            ft.hide(cartFragment);
+        }
+        if (profileFragment.isAdded()) {
+            ft.hide(profileFragment);
+        }
+        ft.commit();
+    }
+
+    private void displaySearch() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (searchFragment.isAdded()) {
+            ft.show(searchFragment);
+        } else {
+            ft.add(R.id.mainFragment, searchFragment);
+        }
+        if (homeFragment.isAdded()) {
+            ft.hide(homeFragment);
+        }
+        if (cartFragment.isAdded()) {
+            ft.hide(cartFragment);
+        }
+        if (profileFragment.isAdded()) {
+            ft.hide(profileFragment);
+        }
+        ft.commit();
+    }
+
+    private void displayCart() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (cartFragment.isAdded()) {
+            ft.show(cartFragment);
+        } else {
+            ft.add(R.id.mainFragment, cartFragment);
+        }
+        if (searchFragment.isAdded()) {
+            ft.hide(searchFragment);
+        }
+        if (homeFragment.isAdded()) {
+            ft.hide(homeFragment);
+        }
+        if (profileFragment.isAdded()) {
+            ft.hide(profileFragment);
+        }
+        ft.commit();
+    }
+
+    private void displayProfile() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (profileFragment.isAdded()) {
+            ft.show(profileFragment);
+        } else {
+            ft.add(R.id.mainFragment, profileFragment);
+        }
+        if (searchFragment.isAdded()) {
+            ft.hide(searchFragment);
+        }
+        if (cartFragment.isAdded()) {
+            ft.hide(cartFragment);
+        }
+        if (homeFragment.isAdded()) {
+            ft.hide(homeFragment);
+        }
+        ft.commit();
     }
 }
